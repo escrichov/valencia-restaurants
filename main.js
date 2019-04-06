c3ced97caa512c122b61f137fc7bd13348f36106
Carlos Escriche <escrichov@gmail.com>
2019-04-06 12:31:54 +0200

    Init

---
+++ main.js
@@ -0,0 +1,55 @@
+function clickEvent (event) {
+    const filter = $( event.target ).data("filter");
+    $( ".filter" ).each(function() {
+        $(this).removeClass('filter-applied');
+    })
+    $( event.target ).addClass('filter-applied');
+
+    if (filter === 'all') {
+        $( ".card" ).each(function() {
+            $(this).parent().show();
+        });
+        return;
+    }
+
+    $( ".card" ).each(function() {
+        var card = $(this);
+        const filters = card.data("filters");
+        var filterExist = false;
+        filters.forEach(function(element) {
+            if (element === filter){
+                filterExist = true;
+            }
+        });
+        if(filterExist){
+            card.parent().show();
+        } else {
+            card.parent().hide();
+        }
+    });
+}
+
+$( document ).ready(function() {
+
+    $( ".filter" ).click(clickEvent);
+    $( ".button-arrow-left" ).click(function(event){
+        var nextImage = $(event.target).parent().data('current-image');
+        const imageList = $(event.target).parent().data('image-list');
+        if (nextImage === 0) {
+            nextImage = imageList.length-1;
+        } else {
+            nextImage = nextImage - 1;
+        }
+        const image = imageList[nextImage];
+        $(event.target).parent().data('current-image', nextImage);
+        $(event.target).parent().css("background-image", 'url(' + image + ')');
+    });
+    $( ".button-arrow-right" ).click(function(event){
+        var nextImage = $(event.target).parent().data('current-image');
+        const imageList = $(event.target).parent().data('image-list');
+        nextImage = (nextImage + 1) % imageList.length;
+        const image = imageList[nextImage];
+        $(event.target).parent().data('current-image', nextImage);
+        $(event.target).parent().css("background-image", 'url(' + image + ')');
+    });
+});
---
+++ main.py
@@ -0,0 +1,91 @@
+import gspread
+import json
+from jinja2 import Environment, FileSystemLoader
+from oauth2client.service_account import ServiceAccountCredentials
+
+# https://developers.google.com/apis-explorer/?hl=es#p/drive/v3/
+# https://developers.google.com/identity/protocols/OAuth2ServiceAccount#delegatingauthority
+# Share document to the email of service account
+# Admin permissions: https://console.cloud.google.com/iam-admin/serviceaccounts?project=food-restaurants-236518
+# gspread https://github.com/burnash/gspread
+# Example template: https://table2site.com/site/valenciafood
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+SPREADSHEET_ID = '1JYX9GT1nfGczuC7DSBTsmimsX3C8J60R5AQW0wBtlrs'
+
+# Authorization
+import time
+start = time.time()
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials_service.json', scope)
+gc = gspread.authorize(credentials)
+
+# Open a worksheet from spreadsheet with one shot
+sh = gc.open_by_key(SPREADSHEET_ID)
+wks = sh.worksheet("DB")
+end = time.time()
+
+class Restaurant():
+    title = None
+    image = None
+    imageplace = None
+    description = None
+    zone = None
+    price = None
+    menu = None
+    tags = []
+    zones = []
+    booking = None
+    web = None
+    tripadvisor = None
+    instagram = None
+    googlemaps = None
+
+    def __init__(self, titles, row):
+        for title, column in zip(titles, row):
+            if title.endswith('-list'):
+                item_list = [item.strip() for item in column.split(',') if item != '']
+                item_list.sort()
+                name = title.replace('-list', '').strip()
+                value = item_list
+            else:
+                name = title.strip()
+                value = column.strip()
+            setattr(self, name, value)
+        self.filters_json = json.dumps(self.tags + self.zones)
+        self.images = []
+        if self.image:
+            self.images.append(self.image)
+        if self.imageplace:
+            self.images.append(self.imageplace)
+        self.images_json = json.dumps(self.images)
+
+
+def get_all_tags(restaurants, tag_column=5):
+    tags = set()
+    for restaurant in restaurants:
+        tags.update(restaurant.tags)
+
+    return list(tags)
+
+def get_all_zones(restaurants, zone_column=3):
+    zones = set()
+    for restaurant in restaurants:
+        zones.update(restaurant.zones)
+
+    return list(zones)
+
+# Get all restaurants
+cell_list = wks.get_all_values()
+titles = cell_list[0]
+restaurants = [Restaurant(titles, row) for row in cell_list[1:]]
+tags = get_all_tags(restaurants)
+tags.sort()
+zones = get_all_zones(restaurants)
+zones.sort()
+
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('main.html')
+output_from_parsed_template = template.render({'restaurants': restaurants, 'tags': tags, 'zones': zones})
+
+with open("index.html", "w") as fh:
+    fh.write(output_from_parsed_template)
