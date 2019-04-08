bc7a371d47be08b783bd6353f46ac505d70fea0a
Carlos Escriche <escrichov@gmail.com>
2019-04-08 22:58:45 +0200

    Sync

--- cron.sh
+++ cron.sh
@@ -12,15 +12,12 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
     ONLY_BUILD_IF_DATA_CHANGES=True pipenv run python main.py
-    exit 1
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
     git pull
     ONLY_BUILD_IF_DATA_CHANGES=False pipenv run python main.py
-    exit 1
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
-    exit 1
     git pull
     git push
     exit 1
