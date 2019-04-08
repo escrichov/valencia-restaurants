ff695bc98f38b58b5ab6f96ce8aabbc11e96373a
Carlos Escriche <escrichov@gmail.com>
2019-04-08 22:57:24 +0200

    Modified cron

--- cron.sh
+++ cron.sh
@@ -4,14 +4,17 @@ DIRECTORY=.
 
 
 cd $DIRECTORY
+git fetch
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
     ONLY_BUILD_IF_DATA_CHANGES=True pipenv run python main.py
     exit 1
 elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
     git pull
     ONLY_BUILD_IF_DATA_CHANGES=False pipenv run python main.py
     exit 1
